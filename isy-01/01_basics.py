e43390d8a5b72f76ff9e694cece3fb7371448a38
Tom Oberhauser <tom@devfoo.de>
2017-10-24 15:41:49 +0200

    Finished e

--- isy-01/01_basics.py
+++ isy-01/01_basics.py
@@ -3,7 +3,6 @@ import cv2
 import math
 import time
 
-
 ######################################################################
 # IMPORTANT: Please make yourself comfortable with numpy and python:
 # e.g. https://www.stavros.io/tutorials/python/
@@ -13,7 +12,8 @@ import time
 # most of the time we'll use np.float32 as arrays
 # e.g. np.float32([0.1,0.1]) equal np.array([1, 2, 3], dtype='f')
 
-print("a) (*) Erzeugen Sie einen Vektor mit Nullen der Länge 10 (10 Elemente) und setzen den Wert des 5.Elementes auf eine 1.")
+print(
+    "a) (*) Erzeugen Sie einen Vektor mit Nullen der Länge 10 (10 Elemente) und setzen den Wert des 5.Elementes auf eine 1.")
 
 vec_a = np.zeros(10)
 vec_a[4] = 1
@@ -21,7 +21,7 @@ print(vec_a)
 
 print("b) (*) Erzeugen Sie einen Vektor mit Ganzahl-Werten von 10 bis 49 (geht in einer Zeile).")
 
-vec_b = np.arange(10,50)
+vec_b = np.arange(10, 50)
 print(vec_b)
 
 print("c) (*) Drehen Sie die Werte des Vektors um (geht in einer Zeile).")
@@ -30,30 +30,35 @@ print(vec_b[::-1])
 
 print("d) (*) Erzeugen Sie eine 4x4 Matrix mit den Werte 0 bis 15 (links oben rechts unten).")
 
-print(np.arange(0,16).reshape(4,4))
+print(np.arange(0, 16).reshape(4, 4))
 
-print("e) (*) Erzeuge eine 8x8 Matrix mit Zufallswerte und finde deren Maximum und Minimum und normalisieren Sie die Werte (sodass alle Werte zwischen 0 und 1 liegen - ein Wert wird 1 (max) sein und einer 0 (min)).")
+print(
+    "e) (*) Erzeuge eine 8x8 Matrix mit Zufallswerte und finde deren Maximum und Minimum und normalisieren Sie die Werte (sodass alle Werte zwischen 0 und 1 liegen - ein Wert wird 1 (max) sein und einer 0 (min)).")
 
-mat_e = np.random.randint(0,255, size=(8,8))
-min_val = mat_e.min()
-max_val = mat_e.max()
+mat_e = np.random.randint(0, 255, size=(8, 8))
+print('Zufallsmatrix:\n')
 print(mat_e)
-print('min:',min_val,'max:',max_val)
-
-# TODO DEN WIRKLICHEN RANGE NORMALISIEREN(ALSO MIN = 0 und MAX = 1)
-
+print('\nmin:', mat_e.min(), 'max:', mat_e.max())
+print("\nNormalisiert:\n")
+mat_e_norm = (mat_e - mat_e.min()) / mat_e.max()
+print(mat_e_norm)
+print('\nmin:', mat_e_norm.min(), 'max:', mat_e_norm.max())
 
 print("f) (*) Multiplizieren Sie eine 4x3 Matrix mit einer 3x2 Matrix")
-print("g) (*) Erzeugen Sie ein 1D Array mit den Werte von 0 bis 20 und negieren Sie Werte zwischen 8 und 16 nachträglich.")
+print(
+    "g) (*) Erzeugen Sie ein 1D Array mit den Werte von 0 bis 20 und negieren Sie Werte zwischen 8 und 16 nachträglich.")
 print("h) (*) Summieren Sie alle Werte in einem Array.")
 print("i) (** ) Erzeugen Sie eine 5x5 Matrix und geben Sie jeweils die geraden und die ungeraden Zeile aus.")
-print("j) (** ) Erzeugen Sie eine Matrix M der Größe 4x3 und einen Vektor v mit Länge 3. Multiplizieren Sie jeden Spalteneintrag aus v mit der kompletten Spalte aus M. Schauen Sie sich dafür an, was Broadcasting in Numpy bedeutet.")
-print("k) (** ) Erzeugen Sie einen Zufallsmatrix der Größe 10x2, die Sie als Kartesische Koordinaten interpretieren können ([[x0, y0],[x1, y1],[x2, y2]]). Konvertieren Sie diese in Polarkoordinaten https://de.wikipedia.org/wiki/Polarkoordinaten.")
-print("l) (***) Implementieren Sie zwei Funktionen, die das Skalarprodukt und die Vektorlänge für Vek- toren beliebiger Länge berechnen. Nutzen Sie dabei NICHT die gegebenen Funktionen von NumPy. Testen Sie Ihre Funktionen mit den gegebenen Vektoren:")
-print("m) (***) Berechnen Sie (v0T v1)Mv0 unter der Nutzung von NumPy Operationen. Achten Sie darauf, dass hier v0,v1 Spaltenvektoren gegeben sind. v0T ist also ein Zeilenvektor.")
+print(
+    "j) (** ) Erzeugen Sie eine Matrix M der Größe 4x3 und einen Vektor v mit Länge 3. Multiplizieren Sie jeden Spalteneintrag aus v mit der kompletten Spalte aus M. Schauen Sie sich dafür an, was Broadcasting in Numpy bedeutet.")
+print(
+    "k) (** ) Erzeugen Sie einen Zufallsmatrix der Größe 10x2, die Sie als Kartesische Koordinaten interpretieren können ([[x0, y0],[x1, y1],[x2, y2]]). Konvertieren Sie diese in Polarkoordinaten https://de.wikipedia.org/wiki/Polarkoordinaten.")
+print(
+    "l) (***) Implementieren Sie zwei Funktionen, die das Skalarprodukt und die Vektorlänge für Vek- toren beliebiger Länge berechnen. Nutzen Sie dabei NICHT die gegebenen Funktionen von NumPy. Testen Sie Ihre Funktionen mit den gegebenen Vektoren:")
+print(
+    "m) (***) Berechnen Sie (v0T v1)Mv0 unter der Nutzung von NumPy Operationen. Achten Sie darauf, dass hier v0,v1 Spaltenvektoren gegeben sind. v0T ist also ein Zeilenvektor.")
 
-
-print(np.float32([0.1,0.1]))
+print(np.float32([0.1, 0.1]))
 
 ######################################################################
 # A2. OpenCV and Transformation and Computer Vision Basic
@@ -71,7 +76,3 @@ print(np.float32([0.1,0.1]))
 # http://docs.opencv.org/3.1.0/da/d54/group__imgproc__transform.html#ga0203d9ee5fcd28d40dbc4a1ea4451983
 # Tip: you need to define a transformation Matrix M
 # see result image
-
-
-
-
