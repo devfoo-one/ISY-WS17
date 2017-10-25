231533541bab0311b6872e6838aabda813c6fdbb
Tom Oberhauser <tom@devfoo.de>
2017-10-25 20:39:42 +0200

    01-2: Implemented image loading and fast grayscale transformation

--- isy-01/01_basics.py
+++ isy-01/01_basics.py
@@ -144,9 +144,6 @@ v1_m = np.array([[-1], [2], [5]])
 #  multiplication featues do not recognize an single element matrix as a scalar.
 print(np.asscalar(np.dot(v0_m.T, v1_m)) * M_m * v0_m)
 
-
-
-
 ######################################################################
 # A2. OpenCV and Transformation and Computer Vision Basic
 
@@ -158,6 +155,33 @@ print(np.asscalar(np.dot(v0_m.T, v1_m)) * M_m * v0_m)
 
 # why Lenna? https://de.wikipedia.org/wiki/Lena_(Testbild)
 
+def bgr2grayscale(img):
+    _img = np.copy(img)
+    height, width = img.shape[:2]
+    for y in range(height):
+        for x in range(width):
+            b, g, r, = _img[y, x]
+            bw = np.uint8(0.72 * b + 0.07 * g + 0.21 * r)
+            _img[y, x] = [bw, bw, bw]
+    return _img
+
+def bgr2grayscale_fast(img):
+    _img = np.copy(img)
+    _img = _img * np.array([[[0.72, 0.07, 0.21]]])
+    bw = np.sum(_img, axis=2)[...,np.newaxis] # equiv. to [:,:,None]
+    _img = np.concatenate((bw,bw,bw), axis=2).astype(np.uint8)
+    return _img
+
+img = cv2.imread('images/Lenna.png')
+img_bw = bgr2grayscale_fast(img)
+display = np.concatenate((img_bw, img), axis=1)
+while True:
+    cv2.imshow('ISY Aufgabe 1', display)
+    key = cv2.waitKey(0)
+    if key == ord('q') or key == ord('Q'):
+        break
+
+cv2.destroyAllWindows()
 # (2) Now shift both images by half (translation in x) it rotate the colored image by 30 degrees using OpenCV transformation functions
 # + do one of the operations on keypress (t - translate, r - rotate, 'q' - quit using cv::warpAffine
 # http://docs.opencv.org/3.1.0/da/d54/group__imgproc__transform.html#ga0203d9ee5fcd28d40dbc4a1ea4451983
