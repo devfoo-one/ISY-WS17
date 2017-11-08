125675e0d6b94a123b8a0ba7b4029285be5d1f2d
Tom Oberhauser <tom@devfoo.de>
2017-11-08 23:36:02 +0100

    Implemented 2.1

--- isy-02/02_features.py
+++ isy-02/02_features.py
@@ -13,3 +13,14 @@ while True:
     # close the window and application by pressing a key
 
     # YOUR CODE HERE
+
+    ret, frame = cap.read()
+    frame_bw = cv2.cvtColor(frame, cv2.COLOR_BGRA2GRAY)
+    sift = cv2.xfeatures2d.SIFT_create()  # code taken from https://docs.opencv.org/3.1.0/da/df5/tutorial_py_sift_intro.html
+    kp = sift.detect(frame_bw)
+    frame_bw = cv2.cvtColor(frame_bw, cv2.COLOR_GRAY2BGR)  # needed to get color markers onto it
+    cv2.drawKeypoints(frame_bw, kp, frame_bw, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    cv2.imshow('Interactive Systems: Towards AR Tracking', frame_bw)
+    key = cv2.waitKey(100)
+    if key == ord('q'):
+        break
