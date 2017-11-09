43d478f98410ab8089f572e96e4a6fb0f2d6fbe0
Tom Oberhauser <tom@devfoo.de>
2017-11-09 14:39:43 +0100

    2.2 WIP - basic stitching implemented (not beautiful)

--- isy-02/02_stitching.py
+++ isy-02/02_stitching.py
@@ -4,7 +4,6 @@ import math
 import sys
 from ImageStitcher import *
 
-
 ############################################################
 #
 #                   Image Stitching
@@ -12,17 +11,27 @@ from ImageStitcher import *
 ############################################################
 
 # 1. load panorama images
-
+pano6 = cv2.imread('images/pano6.jpg')
+pano5 = cv2.imread('images/pano5.jpg')
+pano4 = cv2.imread('images/pano4.jpg')
+pano3 = cv2.imread('images/pano3.jpg')
+pano2 = cv2.imread('images/pano2.jpg')
+pano1 = cv2.imread('images/pano1.jpg')
 
 # order of input images is important is important (from right to left)
-imageStitcher = ImageStitcher([]) # list of images
+# imageStitcher = ImageStitcher([pano4, pano5, pano6]) # list of images
+imageStitcher = ImageStitcher([pano1, pano2, pano3])  # list of images
 (matchlist, result) = imageStitcher.stitch_to_panorama()
 
 if not matchlist:
     print("We have not enough matching keypoints to create a panorama")
 else:
     # YOUR CODE HERE
+
+    for i, match in enumerate(matchlist):
+        cv2.imshow('match' + str(i), match)
     # output all matching images
     # output result
+    cv2.imshow("Result", result)
+    cv2.waitKey(0)
     # Note: if necessary resize the image
-
--- isy-02/ImageStitcher.py
+++ isy-02/ImageStitcher.py
@@ -37,7 +37,7 @@ class ImageStitcher:
             if len(m) == 2 and m[0].distance < m[1].distance * self.distanceRatio:
                 matches.append((m[0].trainIdx, m[0].queryIdx))
 
-        print "matches:", len(matches)
+        print("matches:", len(matches))
         # we need to compute a homography - more next course
         # computing a homography requires at least 4 matches
         if len(matches) > 4:
@@ -70,7 +70,6 @@ class ImageStitcher:
         for ((idx2, idx1), s) in zip(matches, status):
             # only process the match if the keypoint was successfully matched
             if s == 1:
-
                 # x - columns
                 # y - rows
                 (x1, y1) = kp1[idx1].pt
@@ -89,30 +88,67 @@ class ImageStitcher:
                 cv2.line(vis, (int(x1), int(y1)), (int(x2) + w1, int(y2)), (255, 0, 0), 1)
         return vis
 
+    def crop_black(self, img):
+        # code taken from https://stackoverflow.com/questions/39465812/how-to-crop-zero-edges-of-a-numpy-array
+        # crops zero values from image
+        # argwhere will give you the coordinates of every non-zero point
+        true_points = np.argwhere(img)
+        # take the smallest points and use them as the top left of your crop
+        top_left = true_points.min(axis=0)
+        # take the largest points and use them as the bottom right of your crop
+        bottom_right = true_points.max(axis=0)
+        out = img[top_left[0]:bottom_right[0] + 1,  # plus 1 because slice isn't
+              top_left[1]:bottom_right[1] + 1]  # inclusive
+        return out
+
+    def scale_to_width(self, img, width):
+        # proportional scale of an image to a given width
+        h, w = img.shape[:2]
+        h_ = int(h / w * width)
+        return cv2.resize(img, (width, h_))
+
     def stitch_to_panorama(self):
 
         # YOUR CODE HERE
+        matchList = []
+        baseImage = self.imagelist[0]
+        panoramaImg = np.zeros((3000, baseImage.shape[1] * 2, 3), dtype=np.uint8)  ## TODO: adjust size
+        panoramaImg[0:baseImage.shape[0], 0:baseImage.shape[1]] = baseImage
+
         # 1. create feature extraction
+        sift = cv2.xfeatures2d.SIFT_create()
+
         # 2. detect and compute keypoints and descriptors for the first image
+        kp, desc = sift.detectAndCompute(baseImage, None)
 
         # 3. loop through the remaining images and detect and compute keypoints + descriptors
+        for img in self.imagelist[1:]:
+            kp2, desc2 = sift.detectAndCompute(img, None)
+            # 4. match features between the two images consecutive images and check if the result might be None.
+            H, status, matches = self.match_keypoints(kp, kp2, desc, desc2)
+            if not matches:
+                # if not enough matches were found we can't stitch and we break here
+                break
+            else:
+                matchImg = self.draw_matches(baseImage, img, kp, kp2, matches, status)
+                matchList.append(self.scale_to_width(matchImg, 1000))
+                warpedImg = cv2.warpPerspective(img, H, (2000, 2000), flags=cv2.WARP_INVERSE_MAP)
+                panoramaImg[0:warpedImg.shape[0], 0:warpedImg.shape[1]] += warpedImg
 
-        # 4. match features between the two images consecutive images and check if the
-        # result might be None.
 
-        # if not enough matches were found we can't stitch
-        # and we break here
+                # The result contains matches and a status object that can be used to draw the matches.
+                # Additionally (and more importantly it contains the transformation matrix (homography matrix)
+                # commonly refered to as H. That can and should be used with cv2.warpPerspective to transform
+                # consecutive images such that they fit together.
+                # make sure the size of the new (warped) image is large enough to support the overlap
 
-        # The result contains matches and a status object that can be used to draw the matches.
-        # Additionally (and more importantly it contains the transformation matrix (homography matrix)
-        # commonly refered to as H. That can and should be used with cv2.warpPerspective to transform
-        # consecutive images such that they fit together.
-        # make sure the size of the new (warped) image is large enough to support the overlap
+                # the resulting image might be too wide (lot of black areas on the right) because there is a
+                # substantial overlap
 
-        # the resulting image might be too wide (lot of black areas on the right) because there is a
-        # substantial overlap
-
-        # 5. create a new image using draw_matches containing the visualized matches
+                # 5. create a new image using draw_matches containing the visualized matches
 
         # 6. return the resulting stitched image
+
+        panoramaImg = self.crop_black(panoramaImg)
+        panoramaImg = self.scale_to_width(panoramaImg, 1000)
         return (matchList, panoramaImg)
