9a8c7eb679889d1ff686284ae120afc6b098633b
Tom Oberhauser <tom@devfoo.de>
2017-11-16 17:51:48 +0100

    Implemented 2.2 -> Stitching

--- isy-02/02_stitching.py
+++ isy-02/02_stitching.py
@@ -18,20 +18,25 @@ pano3 = cv2.imread('images/pano3.jpg')
 pano2 = cv2.imread('images/pano2.jpg')
 pano1 = cv2.imread('images/pano1.jpg')
 
+stitchers = []
 # order of input images is important is important (from right to left)
 # imageStitcher = ImageStitcher([pano4, pano5, pano6]) # list of images
-imageStitcher = ImageStitcher([pano3, pano2, pano1])  # list of images
-(matchlist, result) = imageStitcher.stitch_to_panorama()
 
-if not matchlist:
-    print("We have not enough matching keypoints to create a panorama")
-else:
-    # YOUR CODE HERE
 
-    for i, match in enumerate(matchlist):
-        cv2.imshow('match' + str(i), match)
-    # output all matching images
-    # output result
-    cv2.imshow("Result", result)
-    cv2.waitKey(0)
-    # Note: if necessary resize the image
+stitchers.append(ImageStitcher([pano6, pano5, pano4]))
+stitchers.append(ImageStitcher([pano3, pano2, pano1]))
+
+for stitcher_i, stitcher in enumerate(stitchers):
+    (matchlist, result) = stitcher.stitch_to_panorama()
+    if not matchlist:
+        print("We have not enough matching keypoints to create a panorama")
+    else:
+        # YOUR CODE HERE
+        for i, match in enumerate(matchlist):
+            cv2.imshow('stitcher {} match {}'.format(stitcher_i, i), match)
+        # output all matching images
+        # output result
+        cv2.imshow('Result stitcher {}'.format(stitcher_i), result)
+
+cv2.waitKey(0)
+# Note: if necessary resize the image
