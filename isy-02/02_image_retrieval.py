b61a0318be8afc8087da620f35b00b400b467677
Tom Oberhauser <tom@devfoo.de>
2017-11-18 12:57:23 +0100

    2.3 - WIP fancy result view

--- isy-02/02_image_retrieval.py
+++ isy-02/02_image_retrieval.py
@@ -4,7 +4,6 @@ import cv2
 import glob
 import numpy as np
 
-
 ############################################################
 #
 #              Simple Image Retrieval
@@ -13,28 +12,31 @@ import numpy as np
 
 sift = cv2.xfeatures2d.SIFT_create()
 
+
 # implement distance function
 def distance(a, b):
     # YOUR CODE HERE
     dist = 0.0
     for vec_a, vec_b in zip(a, b):
-        for elem_a, elem_b in zip(vec_a,vec_b):
-            dist += (elem_a - elem_b)**2
+        for elem_a, elem_b in zip(vec_a, vec_b):
+            dist += (elem_a - elem_b) ** 2
     return int(np.sqrt(dist))
+
 
 def create_keypoints(w, h):
     keypoints = []
     keypointSize = 11
 
     # YOUR CODE HERE
-    stepsN = 11 # how many grid elements should the uniform grid have?
+    stepsN = 15  # how many grid elements should the uniform grid have?
     stepSize = 1.0 / stepsN
-    stepFactors = np.arange(stepSize, 1.0, stepSize)
+    stepFactors = np.arange(0.0, 1.0, stepSize)
     for f_w in stepFactors:
         for f_h in stepFactors:
             k = cv2.KeyPoint(int(w * f_w), int(h * f_h), _size=keypointSize)
             keypoints.append(k)
     return keypoints
+
 
 def getDescriptor(img):
     img_h, img_w = img.shape[:2]
@@ -44,6 +46,35 @@ def getDescriptor(img):
     #    for each keypoint. this compute one descriptor for each image.
     _, descriptors = sift.compute(img, keypoints)
     return descriptors
+
+
+def getFancyResultImage(queryImage, rankedCandidates):
+    # assume that images are squares
+    destinationHeight = 200
+    fancyImage = cv2.resize(queryImage, (destinationHeight, destinationHeight))
+
+    rankedList = []
+    while not rankedCandidates.empty():
+        _, img = rankedCandidates.get() # TODO: BUG: CRASHES AT 3th QUERY WITH
+        # TODO: ValueError: operands could not be broadcast together with shapes (256,256,3) (255,256,3)
+
+        rankedList.append(cv2.resize(img, (int(destinationHeight / 2), int(destinationHeight / 2))))
+
+    # add a black image to avoid IndexError
+    rankedList.append(np.zeros((int(destinationHeight / 2), int(destinationHeight / 2), 3), dtype=np.uint8))
+
+    results = np.zeros((destinationHeight, 0, 3), dtype=np.uint8)
+
+    for i in range(0, int(len(rankedList) / 2)):
+        topImg = rankedList[i]
+        bottomImg = rankedList[i + int(len(rankedList) / 2)]
+        block = np.concatenate((topImg, bottomImg), axis=0)
+        results = np.concatenate((results, block), axis=1)
+
+    fancyImage = np.concatenate((fancyImage, results), axis=1)
+    return fancyImage
+
+
 
 # 1. preprocessing and load
 images_paths = glob.glob('./images/db/*/*.jpg')
@@ -59,9 +90,8 @@ for query_path in glob.glob('./images/db/*.jpg'):
     img = cv2.imread(query_path)
     queries.append((img, getDescriptor(img)))
 
-
-for queryImage, queryDescriptor in queries: # TODO: DEBUG FILTER
-    q = PriorityQueue()
+for queryImage, queryDescriptor in queries:
+    rankedCandidates = PriorityQueue()
     cv2.imshow('query', queryImage)  # TODO: RMD
     for image, descriptor in images:
         # 4. use one of the query input image to query the 'image database' that
@@ -69,15 +99,9 @@ for queryImage, queryDescriptor in queries: # TODO: DEBUG FILTER
         #    compare the descriptor to each image in the database using the L2-norm
         #    and save the result into a priority queue (q = PriorityQueue())
         dist = distance(queryDescriptor, descriptor)
-        q.put((dist, image))  #TODO: BUG, this adds nothing!
+        rankedCandidates.put((dist, image))
 
     # 5. output (save and/or display) the query results in the order of smallest distance
-
-    while not q.empty():
-        dist, img = q.get()
-        cv2.imshow('candidate', img)
-        cv2.waitKey(0)
-
-
-
-
+    cv2.imshow('Fancy Result', getFancyResultImage(queryImage, rankedCandidates))
+    cv2.waitKey(0)
+#
