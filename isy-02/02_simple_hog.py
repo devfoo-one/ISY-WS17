de8b0ae4ade7cd858d0941ef3df155e280b36c25
Tom Oberhauser <tom@devfoo.de>
2017-11-19 13:01:50 +0100

    Begin of 2.4 - implemented mog calculation

--- isy-02/02_simple_hog.py
+++ isy-02/02_simple_hog.py
@@ -2,14 +2,20 @@ import numpy as np
 import cv2
 import math
 import matplotlib
+
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
+
 
 ###############################################################
 #
 # Write your own descriptor / Histogram of Oriented Gradients
 #
 ###############################################################
+
+def imshow(img, title='DEBUG'):  # TODO: RMD
+    cv2.imshow(title, img)
+    cv2.waitKey(0)
 
 
 def plot_histogram(hist, bins):
@@ -20,24 +26,27 @@ def plot_histogram(hist, bins):
 
 
 def compute_simple_hog(imgcolor, keypoints):
-
     # convert color to gray image and extract feature in gray
-
+    imggray = cv2.cvtColor(imgcolor, cv2.COLOR_BGRA2GRAY)
     # compute x and y gradients
-
+    # https://docs.opencv.org/3.0-beta/doc/py_tutorials/py_imgproc/py_gradients/py_gradients.html
+    sobel_x = cv2.Sobel(imggray, cv2.CV_64F, 1, 0)
+    sobel_y = cv2.Sobel(imggray, cv2.CV_64F, 0, 1)
     # compute magnitude and angle of the gradients
+    mog = np.uint8(np.sqrt(sobel_x ** 2 + sobel_y ** 2))
+    imshow(mog) # TODO RMD
 
     # go through all keypoints and and compute feature vector
     descr = np.zeros((len(keypoints), 8), np.float32)
     count = 0
     for kp in keypoints:
-        # print kp.pt, kp.size
+        print(kp.pt, kp.size)
         # extract angle in keypoint sub window
         # extract gradient magnitude in keypoint subwindow
 
         # create histogram of angle in subwindow BUT only where magnitude of gradients is non zero! Why? Find an
         # answer to that question use np.histogram
-        #(hist, bins) = np.histogram(...)
+        # (hist, bins) = np.histogram(...)
 
         plot_histogram(hist, bins)
 
@@ -49,6 +58,5 @@ def compute_simple_hog(imgcolor, keypoints):
 keypoints = [cv2.KeyPoint(15, 15, 11)]
 
 # test for all test images
-test = cv2.imread('./images/hog_test/diag.jpg')
+test = cv2.imread('./images/hog_test/circle.jpg')
 descriptor = compute_simple_hog(test, keypoints)
-
