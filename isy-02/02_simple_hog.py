514a9407640a2dd398c5b11920ec7ed6390ad2e1
Tom Oberhauser <tom@devfoo.de>
2017-11-19 14:54:52 +0100

    Issue #1

--- isy-02/02_simple_hog.py
+++ isy-02/02_simple_hog.py
@@ -13,11 +13,6 @@ import matplotlib.pyplot as plt
 #
 ###############################################################
 
-def imshow(img, title='DEBUG'):  # TODO: RMD
-    cv2.imshow(title, img)
-    cv2.waitKey(0)
-
-
 def plot_histogram(hist, bins):
     width = 0.7 * (bins[1] - bins[0])
     center = (bins[:-1] + bins[1:]) / 2
@@ -29,6 +24,7 @@ def normalize(arr):
 
 def compute_simple_hog(imgcolor, keypoints):
     # convert color to gray image and extract feature in gray
+
     imggray = cv2.cvtColor(imgcolor, cv2.COLOR_BGRA2GRAY)
     # compute x and y gradients
     # https://docs.opencv.org/3.0-beta/doc/py_tutorials/py_imgproc/py_gradients/py_gradients.html
@@ -36,28 +32,23 @@ def compute_simple_hog(imgcolor, keypoints):
     sobel_y = cv2.Sobel(imggray, cv2.CV_64F, 0, 1)
     # compute magnitude and angle of the gradients
     mog = cv2.magnitude(sobel_x, sobel_y)
-    # imshow(np.uint8(mog))  # TODO RMD
 
     # go through all keypoints and and compute feature vector
     descr = np.zeros((len(keypoints), 8), np.float32)
     for count, kp in enumerate(keypoints):
-        print(kp.pt, kp.size)
         kp_y, kp_x = kp.pt
         y1 = int(kp_y - int(kp.size / 2))
         y2 = int(kp_y + int(kp.size / 2))
         x1 = int(kp_x - int(kp.size / 2))
         x2 = int(kp_x + int(kp.size / 2))
-        # mog_sub = mog[y1:y2, x1:x2]  # TODO RMD
-        # imshow(np.uint8(mog_sub))  # TODO RMD
         # extract angle in keypoint sub window
         # extract gradient magnitude in keypoint subwindow
-
         # create histogram of angle in subwindow BUT only where magnitude of gradients is non zero! Why? Find an
         # answer to that question use np.histogram
         # (hist, bins) = np.histogram(...)
         # Answer: if mog = 0, cv2.phase returns a 100% phase of 0 rad which means full angle at 0deg (not 0)
         angle = cv2.phase(sobel_x[y1:y2, x1:x2], sobel_y[y1:y2, x1:x2])
-        (hist, bins) = np.histogram(angle, 9)
+        (hist, bins) = np.histogram(angle, 8)
         hist = normalize(hist)
         plot_histogram(hist, bins)
         if (np.max(mog[y1:y2, x1:x2] > 0)):
@@ -70,5 +61,11 @@ def compute_simple_hog(imgcolor, keypoints):
 keypoints = [cv2.KeyPoint(15, 15, 11)]
 
 # test for all test images
-test = cv2.imread('./images/hog_test/circle.jpg')
-descriptor = compute_simple_hog(test, keypoints)
+testImages = []
+# testImages.append(cv2.imread('./images/hog_test/diag.jpg'))
+testImages.append(cv2.imread('./images/hog_test/vert.jpg'))
+testImages.append(cv2.imread('./images/hog_test/horiz.jpg'))
+# testImages.append(cv2.imread('./images/hog_test/circle.jpg'))
+for test in testImages:
+    descriptor = compute_simple_hog(test, keypoints)
+    print(descriptor)
