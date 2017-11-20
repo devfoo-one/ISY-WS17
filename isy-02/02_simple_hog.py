fa5977c46a710061b28183559f5f5cfd84ba3daf
Tom Oberhauser <tom@devfoo.de>
2017-11-20 18:18:57 +0100

    Implemented 2.4, closes #2

--- isy-02/02_simple_hog.py
+++ isy-02/02_simple_hog.py
@@ -34,44 +34,46 @@ def compute_simple_hog(imgcolor, keypoints):
     sobel_y = cv2.Sobel(imggray, cv2.CV_64F, 0, 1)
     # compute magnitude and angle of the gradients
     mog = cv2.magnitude(sobel_x, sobel_y)
+    mog = np.uint8(mog)
+
+    # test images are noisy jpegs.. so thresholding leads to better results
+    # _, mog = cv2.threshold(mog, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
     # go through all keypoints and and compute feature vector
     descr = np.zeros((len(keypoints), 8), np.float32)
     for count, kp in enumerate(keypoints):
         kp_y, kp_x = kp.pt
         y1 = int(kp_y - int(kp.size / 2))
-        y2 = int(kp_y + int(kp.size / 2))
+        y2 = int(kp_y + int(kp.size / 2)) + 1
         x1 = int(kp_x - int(kp.size / 2))
-        x2 = int(kp_x + int(kp.size / 2))
+        x2 = int(kp_x + int(kp.size / 2)) + 1
         # extract angle in keypoint sub window
         # extract gradient magnitude in keypoint subwindow
         # create histogram of angle in subwindow BUT only where magnitude of gradients is non zero! Why? Find an
         # answer to that question use np.histogram
         # (hist, bins) = np.histogram(...)
-        # Answer: if mog = 0, cv2.phase returns a 100% phase of 0 rad which means full vertical angle at 0deg (not 0)
+        # Answer: Where there is no gradient, angle returns 0 (instead of NaN or None) which means an angle of 0 or 360 degrees.
 
         mog_window = mog[y1:y2, x1:x2]
         sobel_x_window = sobel_x[y1:y2, x1:x2]
         sobel_y_window = sobel_y[y1:y2, x1:x2]
-        sobel_x_window_nonzero = sobel_x_window[np.nonzero(mog_window)]
-        sobel_y_window_nonzero = sobel_y_window[np.nonzero(mog_window)]
-        angle = cv2.phase(sobel_x_window_nonzero, sobel_y_window_nonzero)
-
+        angle = cv2.phase(sobel_x_window, sobel_y_window)
+        angle = angle[mog_window != 0]
         (hist, bins) = np.histogram(angle, bins=8, range=(0.0, 2 * np.pi), density=True)
-        # hist = normalize(hist)
         plot_histogram(hist, bins)
         descr[count] = hist
 
     return descr
 
 
-keypoints = [cv2.KeyPoint(15, 15, 11)]
+keypoints = [cv2.KeyPoint(15, 15, 20)]
 
 # test for all test images
 testImages = []
-# testImages.append(cv2.imread('./images/hog_test/diag.jpg'))
-# testImages.append(cv2.imread('./images/hog_test/horiz.jpg'))
+testImages.append(cv2.imread('./images/hog_test/diag.jpg'))
+testImages.append(cv2.imread('./images/hog_test/horiz.jpg'))
 testImages.append(cv2.imread('./images/hog_test/vert.jpg'))
-# testImages.append(cv2.imread('./images/hog_test/circle.jpg'))
+testImages.append(cv2.imread('./images/hog_test/vert.bmp'))
+testImages.append(cv2.imread('./images/hog_test/circle.jpg'))
 for test in testImages:
     descriptor = compute_simple_hog(test, keypoints)
