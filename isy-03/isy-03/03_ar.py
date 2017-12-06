339728ed7a4fff1881f42dc88d2c89ac0e28556b
Tom Oberhauser <tom@devfoo.de>
2017-12-06 13:37:44 +0100

    3.4 - WIP

--- isy-03/isy-03/03_ar.py
+++ isy-03/isy-03/03_ar.py
@@ -26,8 +26,33 @@ def render_virtual_object(img, x_start, y_start, x_end, y_end, quad):
     # define vertices, edges and colors of your 3D object, e.g. cube
 
     # YOUR CODE HERE
-    # vertices = np.float32([[x, y, z,][x, y, z]])
-    # edges = [(0, 1), ...]
+    z_offset = 1
+    vertices = np.float32([
+        [x_start, y_start, 0],  #0
+        [1, y_start, 0],    #1
+        [1, 1, 0],      #2
+        [x_start, 1, 0],    #3
+        [x_start, y_start, z_offset],  #4
+        [1, y_start, z_offset],    #5
+        [1, 1, z_offset],      #6
+        [x_start, 1, z_offset],    #7
+    ]
+    )
+
+    edges = [
+        (0, 1),
+        (1, 2),
+        (2, 3),
+        (3, 0),
+        (0, 4), # bridge
+        (1, 5), # bridge
+        (2, 6), # bridge
+        (3, 7), # bridge
+        (4, 5),
+        (5, 6),
+        (6, 7),
+        (7, 4),
+       ]
 
     color_lines = (0, 0, 0)
 
@@ -44,10 +69,13 @@ def render_virtual_object(img, x_start, y_start, x_end, y_end, quad):
     # find object pose from 3D-2D point correspondences of the 3d quad using Levenberg-Marquardt optimization
     # in order to work we need K (given above and YOUR distortion coefficients from Assignment 2 (camera calibration))
     # YOUR VALUES HERE
-    #dist_coef = np.array([])
+    dist_coef = None
+    # dist_coef = np.array([0.206366627977, 0.70153901889, 0.00791267216272, -0.0270341602594, -1.91608092802])
 
     # compute extrinsic camera parameters using cv2.solvePnP
     # YOUR CODE HERE
+    # _, rvec, tvec = cv2.solvePnP(quad_3d, quad, K, dist_coef, flags=cv2.SOLVEPNP_ITERATIVE)
+    _, rvec, tvec = cv2.solvePnP(quad_3d, quad, K, dist_coef, flags=cv2.SOLVEPNP_ITERATIVE)
 
     # transform vertices: scale and translate form 0 - 1, in window size of the marker
     scale = [x_end-x_start, y_end-y_start, x_end-x_start]
@@ -58,6 +86,8 @@ def render_virtual_object(img, x_start, y_start, x_end, y_end, quad):
     # call cv2.projectPoints with verts, and solvePnP result, K, and dist_coeff
     # returns a tuple that includes the transformed vertices as a first argument
     # YOUR CODE HERE
+
+    verts, _ = cv2.projectPoints(verts, rvec, tvec, K, dist_coef)
 
     # we need to reshape the result of projectPoints
     verts = verts.reshape(-1, 2)
@@ -74,6 +104,7 @@ cv2.namedWindow('Interactive Systems: AR Tracking')
 while True:
     # YOUR CODE
     _, vis = cap.read()
+    # vis = cv2.imread('DEBUGIMAGE.jpg') ## TODO REMOVE ME DEBUG!
     # detect and compute descriptor in camera image
     keypointsFrame, descriptorsFrame = sift.detectAndCompute(vis, None)
     if descriptorsMarker is None or descriptorsFrame == None:
@@ -106,7 +137,7 @@ while True:
     # H - homography matrix
     # status - status about inliers and outliers for the plane mapping
     # YOUR CODE
-    (H, status) = cv2.findHomography(p0, p1, cv2.RANSAC, 4.0)
+    (H, status) = cv2.findHomography(p0, p1, cv2.RANSAC)
 
     # on the basis of the status object we can now filter RANSAC outliers
     mask = status.ravel() != 0
@@ -119,7 +150,14 @@ while True:
 
     # get the size of the marker and form a quad in pixel coords np float array using w/h as the corner points
     # YOUR CODE HERE
-    # TODO HIER WEITER
+    h1, w1 = markerImg.shape[:2]
+    quad = np.array(
+    [np.array([0, 0], dtype=np.float32), ## UL
+    np.array([0, h1], dtype=np.float32), ## UR
+    np.array([w1, h1], dtype=np.float32), ## LR
+    np.array([w1, 0], dtype=np.float32)] ## LL
+    )
+
 
     # perspectiveTransform needs a 3-dimensional array
     quad = np.array([quad])
@@ -134,4 +172,7 @@ while True:
     render_virtual_object(vis, 0, 0, h1, w1, quad)
 
     cv2.imshow('Interactive Systems: AR Tracking', vis)
+    key = cv2.waitKey(10)
+    if key == ord('q'):
+        break
 
